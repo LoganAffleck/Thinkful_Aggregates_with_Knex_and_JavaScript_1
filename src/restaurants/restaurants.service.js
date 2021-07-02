ce93efb181f23c9b9a49532d72aae2874090e201
J. Logan Affleck <affleck.logan@gmail.com>
2021-07-02 09:47:19 -0600

    Initial Upload

---
+++ src/restaurants/restaurants.service.js
@@ -0,0 +1,54 @@
+const knex = require("../db/connection");
+
+function averageRating() {
+  return knex("restaurants")
+    .avg("rating")
+  
+}
+
+function count() {
+  return knex("restaurants")
+    .count("restaurant_id")
+}
+
+function create(newRestaurant) {
+  return knex("restaurants")
+    .insert(newRestaurant, "*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+function destroy(restaurant_id) {
+  return knex("restaurants").where({ restaurant_id }).del();
+}
+
+function list() {
+  return knex("restaurants").select("*");
+}
+
+function read(restaurant_id) {
+  return knex("restaurants").select("*").where({ restaurant_id }).first();
+}
+
+function readHighestRated() {
+  return knex("restaurants")
+  .select("*")
+  .where({rating : 5})
+}
+
+function update(updatedRestaurant) {
+  return knex("restaurants")
+    .select("*")
+    .where({ restaurant_id: updatedRestaurant.restaurant_id })
+    .update(updatedRestaurant, "*");
+}
+
+module.exports = {
+  averageRating,
+  count,
+  create,
+  delete: destroy,
+  list,
+  read,
+  readHighestRated,
+  update,
+};
